91a8d67bfc3069039cc72052c4b70a8eb2b84df3
MarinTheDuck <72809122+MarinTheDuck@users.noreply.github.com>
2023-05-16 10:07:26 +0000

    First working version.

--- background.js
+++ background.js
@@ -2,8 +2,13 @@ chrome.webRequest.onBeforeRequest.addListener(
   (details) => {
     const { url } = details;
     if (url.includes("google.com/sorry/index")) {
-      return {
-        redirectUrl: "https://duckduckgo.com/" + encodeURIComponent(url),
+        let searchQuery = url
+          .split("www.google.com/search%3Fq%3D")[1]
+          .split("%26source%3D")[0];
+
+        //alert("Google is blocking your search for " + searchQuery + ". Redirecting to DuckDuckGo...");
+        return {
+        redirectUrl: "https://duckduckgo.com/?q=" + searchQuery,
       };
     }
   },
